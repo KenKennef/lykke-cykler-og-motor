668d0ef09e3d0b8832081e3fdc61fc252c2e2cd7
Kenneth Voss <kevo@bluefragments.com>
2019-03-07 17:19:24 +0100

    removed console.log

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 $(window).on('load', function() {
     $('.product > .image > .Photo3_Frontpage > a > img, .product > .image > .Photo3_Frontpage > a > .Element_Overlay_DIV > .Element_Overlay_Frontpage_SavingsDIV > img').map(function(i, image) {
-        console.log(image)
         $(image).css({
             'height': '187.5px', 
             'width': '250px', 
