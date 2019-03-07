9180e4bb554b7a087a55f3b68bf56c89963eecfc
Kenneth Voss <kevo@bluefragments.com>
2019-03-07 17:18:30 +0100

    added static ratios in css

--- test.js
+++ test.js
@@ -1,6 +1,16 @@
 $(window).on('load', function() {
     $('.product > .image > .Photo3_Frontpage > a > img, .product > .image > .Photo3_Frontpage > a > .Element_Overlay_DIV > .Element_Overlay_Frontpage_SavingsDIV > img').map(function(i, image) {
         console.log(image)
-        $(image).css({'height': '187.5px', 'width': '250px', 'max-height': '187.5px', 'max-width': '250px'});
+        $(image).css({
+            'height': '187.5px', 
+            'width': '250px', 
+            'min-height': '187.5px', 
+            'min-width': '250px', 
+            'max-height': '187.5px', 
+            'max-width': '250px',
+            // '-webkit-box-shadow': '5px 5px 10px -5px rgba(140,137,140,1)',
+            // '-moz-box-shadow': '5px 5px 10px -5px rgba(140,137,140,1)',
+            // 'box-shadow': '5px 5px 10px -5px rgba(140,137,140,1)'
+        });
     })
 })
